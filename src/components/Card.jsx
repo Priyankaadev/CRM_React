aa183631d1c6100ed0ee90b1f63f42b55c4f0fc7
Priyankaadev <priyaaswal79@gmail.com>
2024-06-27 21:47:49 +0530

    reusable card component created

---
+++ src/components/Card.jsx
@@ -0,0 +1,24 @@
+function Card({ children, fontColor='text-white', background="bg-primary",divider="bg-white",borderColor="border-error",titleText= "Card", status=50, quantity=50 }) {
+    return (
+        <div className={`w-64 h-52 border-b-8 ${borderColor}  rounded-md flex flex-col ${background} justify-start items-center py-4`}>
+
+            <div className="text-primary-content text-2xl mb-2">
+                {children}
+            <span>{titleText}</span>
+            </div>
+            <div className={`divider ${divider}   h-0.5 mx-4 rounded-sm`}></div>
+            <div className="flex justify-around mt-2 items-center">
+                <div className={`text-5xl ${fontColor} `}>
+                    {quantity}
+                </div>
+               
+                    <div className={`radial-progress l ${fontColor}`}  style={{ "--value": status }}>
+                        {status}%
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Card;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,18 @@
+// import Card from '../../components/Card';
 import Homelayout from '../../layouts/Homelayout';
+// import { BsFillPencilFill } from "react-icons/bs";
 
-function Home( ){
-    return ( 
-       <Homelayout>
-     hello ji
-       </Homelayout>
+
+function Home() {
+    return (
+        <Homelayout>
+            {/* <Card>
+                <BsFillPencilFill className="inline mr-2  " />
+            </Card>
+            <Card background='bg-yellow-200' fontColor='text-black' divider='bg-black' borderColor='border-success'>
+                <BsFillPencilFill className="inline mr-2  " />
+            </Card> */}
+        </Homelayout>
     );
 }
 
